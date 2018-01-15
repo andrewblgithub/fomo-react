c1207e6f6eb22c3e264b354424afa0b9dece8409
Your Name <you@example.com>
2018-01-15 11:35:33 -0500

    heroku

--- db/config.js
+++ db/config.js
@@ -1,12 +1,24 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
-  database: 'fomo',
-  username: 'andrew',
-  password: null,
-  dialect: 'postgres',
-  host: process.env.DATABASE_URL || 'localhost'
+var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+const sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging: false,
+    dialectOptions: {
+        ssl: true
+    }
 });
+
+// const sequelize = new Sequelize({
+//   database: 'fomo',
+//   username: 'andrew',
+//   password: null,
+//   dialect: 'postgres',
+//   host: process.env.DATABASE_URL || 'localhost'
+// });
 
 sequelize
   .authenticate()
